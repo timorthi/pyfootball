67c22128b7e5b3e060073cd53b62c6582c4d2500
Timothy Ng <timnjj@gmail.com>
2016-10-14 14:17:09 -0700

    Completed Player object wrapper

--- pyfootball/models/player.py
+++ pyfootball/models/player.py
@@ -1,3 +1,15 @@
+import traceback
+
+
 class Player():
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        try:
+            self.name = data['name']
+            self.position = data['position']
+            self.jersey_number = data['jerseyNumber']
+            self.date_of_birth = data['dateOfBirth']
+            self.nationality = data['nationality']
+            self.contract_until = data['contractUntil']
+            self.market_value = data['marketValue']
+        except KeyError:
+            traceback.print_exc()
