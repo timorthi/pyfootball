675b8cfb8d884d088d122476b9650a68e5ada505
Timothy Ng <timnjj@gmail.com>
2016-10-14 14:17:48 -0700

    Implemented Team.get_players()

--- pyfootball/endpoints.py
+++ pyfootball/endpoints.py
@@ -1,3 +1,4 @@
 ALL_COMPETITIONS = 'http://api.football-data.org/v1/competitions/'
 
-TEAM = 'http://api.football-data.org/v1/teams/{0}'  # id
+TEAM = 'http://api.football-data.org/v1/teams/{0}'  # team id
+PLAYERS = 'http://api.football-data.org/v1/teams/{0}/players'  # team id
