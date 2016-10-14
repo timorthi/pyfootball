dcb1603e0cada70536a272abd668da67b6f07441
Timothy Ng <timnjj@gmail.com>
2016-10-14 10:02:55 -0700

    Completed data wrapping. Added skeletons for get_fixtures() and get_players()

--- pyfootball/models/team.py
+++ pyfootball/models/team.py
@@ -1,3 +1,27 @@
+import traceback
+
 class Team(object):
-    def __init__(self):
+    def __init__(self, data, id):
+        """ Takes a dict converted from the JSON response by the API and wraps
+            the team data with an object.
+
+            Keyword arguments:
+            data -- A python dict converted from JSON containing the team data.
+        """
+        try:
+            self._fixtures_ep = data['_links']['fixtures']['href']
+            self._players_ep = data['_links']['players']['href']
+            self.name = data['name']
+            self.code = data['code']
+            self.short_name = data['shortName']
+            self.market_value = data['squadMarketValue']
+            self.crest_url = data['crestUrl']
+            self.id = id
+        except KeyError:
+            traceback.print_exc()
+
+    def get_fixtures(self):
+        pass
+
+    def get_players(self):
         pass
