2839ee7fd883ff2a1788f93e3ad4674707ffe4fc
Timothy Ng <timnjj@gmail.com>
2016-10-18 21:15:19 -0700

    Added a file to contain mock data

---
+++ tests/resources.py
@@ -0,0 +1,9 @@
+PLAYER = {
+        "name": "Paul Pogba",
+        "position": "Central Midfield",
+        "jerseyNumber": 6,
+        "dateOfBirth": "1993-03-15",
+        "nationality": "France",
+        "contractUntil": "2021-06-30",
+        "marketValue": "70,000,000 €"
+    }
