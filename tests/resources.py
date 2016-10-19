24cb870b66093ceeddf2b12c909168bd922a0da8
Timothy Ng <timnjj@gmail.com>
2016-10-18 21:55:46 -0700

    Added LeagueTable test

--- tests/resources.py
+++ tests/resources.py
@@ -1,9 +1,119 @@
 PLAYER = {
-        "name": "Paul Pogba",
-        "position": "Central Midfield",
-        "jerseyNumber": 6,
-        "dateOfBirth": "1993-03-15",
-        "nationality": "France",
-        "contractUntil": "2021-06-30",
-        "marketValue": "70,000,000 €"
+      "name": "Paul Pogba",
+      "position": "Central Midfield",
+      "jerseyNumber": 6,
+      "dateOfBirth": "1993-03-15",
+      "nationality": "France",
+      "contractUntil": "2021-06-30",
+      "marketValue": "70,000,000 €"
+    }
+    
+LEAGUE_TABLE = {
+  "_links": {
+    "self": {
+      "href": "http://api.football-data.org/v1/competitions/430/leagueTable/?matchday=7"
+    },
+    "competition": {
+      "href": "http://api.football-data.org/v1/competitions/430"
+    }
+  },
+  "leagueCaption": "1. Bundesliga 2016/17",
+  "matchday": 7,
+  "standing": [
+    {
+      "_links": {
+        "team": {
+          "href": "http://api.football-data.org/v1/teams/5"
+        }
+      },
+      "position": 1,
+      "teamName": "FC Bayern München",
+      "crestURI": "http://upload.wikimedia.org/wikipedia/commons/c/c5/Logo_FC_Bayern_München.svg",
+      "playedGames": 7,
+      "points": 17,
+      "goals": 18,
+      "goalsAgainst": 4,
+      "goalDifference": 14,
+      "wins": 5,
+      "draws": 2,
+      "losses": 0,
+      "home": {
+        "goals": 13,
+        "goalsAgainst": 2,
+        "wins": 3,
+        "draws": 1,
+        "losses": 0
+      },
+      "away": {
+        "goals": 5,
+        "goalsAgainst": 2,
+        "wins": 2,
+        "draws": 1,
+        "losses": 0
+      }
+    },
+    {
+      "_links": {
+        "team": {
+          "href": "http://api.football-data.org/v1/teams/1"
+        }
+      },
+      "position": 2,
+      "teamName": "1. FC Köln",
+      "crestURI": "http://upload.wikimedia.org/wikipedia/de/1/16/1._FC_Köln.svg",
+      "playedGames": 7,
+      "points": 15,
+      "goals": 12,
+      "goalsAgainst": 4,
+      "goalDifference": 8,
+      "wins": 4,
+      "draws": 3,
+      "losses": 0,
+      "home": {
+        "goals": 8,
+        "goalsAgainst": 2,
+        "wins": 3,
+        "draws": 1,
+        "losses": 0
+      },
+      "away": {
+        "goals": 4,
+        "goalsAgainst": 2,
+        "wins": 1,
+        "draws": 2,
+        "losses": 0
+      }
+    },
+    {
+      "_links": {
+        "team": {
+          "href": "http://api.football-data.org/v1/teams/721"
+        }
+      },
+      "position": 3,
+      "teamName": "Red Bull Leipzig",
+      "crestURI": "http://upload.wikimedia.org/wikipedia/de/d/d4/RB_Leipzig_2010_logo.svg",
+      "playedGames": 7,
+      "points": 15,
+      "goals": 12,
+      "goalsAgainst": 5,
+      "goalDifference": 7,
+      "wins": 4,
+      "draws": 3,
+      "losses": 0,
+      "home": {
+        "goals": 4,
+        "goalsAgainst": 2,
+        "wins": 2,
+        "draws": 1,
+        "losses": 0
+      },
+      "away": {
+        "goals": 8,
+        "goalsAgainst": 3,
+        "wins": 2,
+        "draws": 2,
+        "losses": 0
+      }
+    }]
     }
